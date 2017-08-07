8f1a8a8983a7bb05cb2ee25684a60ea4c180567c
Jonathan Freedman <jonathan.freedman@autodesk.com>
2017-08-07 14:04:28 -0700

    add some travis and maybe a pypi

--- Makefile
+++ Makefile
@@ -3,4 +3,10 @@ test:
 	pylint --rcfile=/dev/null propriecle
 	vulture propriecle propriecle.py
 
+version:
+	cp version propriecle/version
+
+package: version
+	python setup.py sdist
+
 .PHONY: test
--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+import os
+import sys
 from setuptools import setup
+from pkg_resources import resource_filename
+
+# depending on your execution context the version file
+# may be located in a different place!
+vsn_path = resource_filename(__name__, 'propriecle/version')
+if not os.path.exists(vsn_path):
+    vsn_path = resource_filename(__name__, 'version')
+    if not os.path.exists(vsn_path):
+        print("%s is missing" % vsn_path)
+        sys.exit(1)
 
 setup(name='propriecle',
-      version='0.0.0',
+      version=open(vsn_path, 'r').read(),
       description='something something vault master keys',
       author='Jonathan Freedman',
       author_email='jonathan.freedman@autodesk.com',
@@ -13,5 +25,6 @@ setup(name='propriecle',
       packages=['propriecle'],
       entry_points={
           'console_scripts': ['propriecle = propriecle.cli:main']
-      }
+      },
+      package_data={'propriecle': ['version']}
 )
